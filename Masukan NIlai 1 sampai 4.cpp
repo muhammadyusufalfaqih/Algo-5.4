c150fb46f0818ad8a55a595ebb2a5aafe061f3f6
muhammadyusufalfaqih <45525489+muhammadyusufalfaqih@users.noreply.github.com>
2018-12-30 15:38:45 -0800

    Add files via upload

---
+++ Masukan NIlai 1 sampai 4.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int tukar (int nilai);
+int main (void)
+{
+    int nilai ;
+    printf (" Masukan NIlai 1 sampai 4 : ");
+    scanf ("%d",&nilai);
+    tukar (nilai);
+    getch();
+}
+int tukar (int nilai)
+{
+    switch (nilai)
+    {
+    case 1 :
+        printf(" Nilai Huruf : A");break;
+    case 2 :
+        printf(" Nilai Huruf : B");break;
+    case 3 :
+        printf(" Nilai Huruf : C");break;
+    case 4 :
+        printf(" Nilai Huruf : D");break;
+    default :
+        printf(" Kesalahan Input Angka");
+    }
+    return (nilai);
+}
